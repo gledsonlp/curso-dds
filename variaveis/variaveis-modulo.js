6d24830bd4c6a545e300d8730cf3925069a74962
Gledson L Pereira <gledsonlp84@gmail.com>
2023-01-17 18:13:57 -0300

    Finalização das aulas do dia

---
+++ variaveis/variaveis-modulo.js
@@ -0,0 +1,3 @@
+let resto = 23 % 5; //3
+
+console.log(resto);
---
+++ variaveis/variaveis-sqrt-pow.js
@@ -0,0 +1,6 @@
+let raiz = Math.sqrt(16);
+console.log(raiz);
+
+//let potencia = Math.pow(2, 3);
+let potencia = 2 ** 3; // outra forma de se calcular a potência
+console.log(potencia);
