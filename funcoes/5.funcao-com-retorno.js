811985f3443d759dad20a7773b574d00785b4639
Gledson L Pereira <gledsonlp@icloud.com>
2023-02-27 15:01:11 -0300

    Delete funcoes directory

--- funcoes/5.funcao-com-retorno.js
+++
@@ -1,9 +0,0 @@
-//verificar se uma pessoa é maior de idade
-
-function verificarMaiorIdade(idade) {
-    return (idade >= 18);
-}
-
-const valorRetornadoPelaFuncao = verificarMaiorIdade(20);
-
-console.log(valorRetornadoPelaFuncao);
--- funcoes/9.escopo.js
+++
@@ -1,8 +0,0 @@
-
-let z = 5;
-
-function soma(x, y) {
-    return x + y + z; // funciona mas não é bom acessar variável fora do escopo da função.
-}
-
-console.log(soma(5, 3));
