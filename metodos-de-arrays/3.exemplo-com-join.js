52f8712cc8c2afb290eb83dff22710c54523d39e
Gledson L Pereira <gledsonlp@icloud.com>
2023-02-27 15:37:51 -0300

    Delete metodos-de-arrays directory

--- metodos-de-arrays/3.exemplo-com-join.js
+++
@@ -1,8 +0,0 @@
-function inverterString(texto) {
-    const arrayDeLetras = texto.split('');
-    arrayDeLetras.reverse();
-    let textoInvertido = arrayDeLetras.join('');
-    console.log(textoInvertido);
-}
-
-inverterString("Cubos Academy");
