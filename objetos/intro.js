597dd932bae747fd20748ba636e7ff25b7850824
Gledson L Pereira <gledsonlp@icloud.com>
2023-02-27 15:37:59 -0300

    Delete objetos directory

--- objetos/intro.js
+++
@@ -1,9 +0,0 @@
-const pessoa = {
-    nome: "José",
-    altura: 1.75,
-    peso: 105
-}
-
-pessoa["nome"] = "Gledson"; // outra forma de se referir ao atributo de um objeto
-
-console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.altura} metros e peso ${pessoa.peso}kg`);
